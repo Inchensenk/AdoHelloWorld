ac966b36a52260342ecde1f65711ae709a0a890f
sofoyan gevork <aab81@bk.ru>
2022-09-24 16:48:52 +0300

    1

--- AdoHelloWorld/MainWindowViewModel.cs
+++ AdoHelloWorld/MainWindowViewModel.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace AdoHelloWorld
 {
+    
+    // INotifyPropertyChanged связывает между собой вьюху, модель и вьюмодель
+    
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -35,28 +39,57 @@ namespace AdoHelloWorld
                 //ожидает в текущем потоке пока выполнится другой поток
                 await connection.OpenAsync();
 
-                MessageBox.Show($"Connected: {connection.ClientConnectionId/*id сеанса*/}");
+                //MessageBox.Show($"Connected: {connection.ClientConnectionId/*id сеанса*/}");
 
-                SqlCommand command = new SqlCommand("SELECT * FROM Persons", connection);
+                //SqlCommand command = new SqlCommand("SELECT * FROM Persons", connection);
 
-               
 
-                //получение специфичного класса для считывания данных типо потока
-                using (SqlDataReader reader = await command.ExecuteReaderAsync())
+
+                ////получение специфичного класса для считывания данных типо потока
+                //using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                //{
+                //    //Усли есть что считывать то ReadAsync() возвращает true иначе false и выход из блока кода
+                //    while (await reader.ReadAsync())
+                //    {
+                //        var person = new Person
+                //        {
+                //            Id = reader.GetInt32(0),
+                //            Name = reader.GetString(1),
+                //            Age = reader.GetInt32(2),
+                //            Height = reader.GetDouble(3)
+                //        };
+                //        People.Add(person);
+                //    }
+                //}
+
+                //SqlCommand command = new SqlCommand($"CREATE TABLE Users (Id INT PRIMARY KEY IDENTITY, Age INT NOT NULL, Name NVARCHAR(100) NOT NULL)", connection);
+
+                //SqlCommand command = new SqlCommand("INSERT INTO Persons (Name, Age, Height) VALUES ('Владимир', 30, 190.1), ('Елена', 28, 165.5)", connection);
+
+                //SqlCommand command = new SqlCommand("UPDATE Persons SET Age = 40 WHERE Name ='Елена'", connection);
+
+                SqlCommand command = new SqlCommand("DELETE FROM Persons WHERE Age >= 30", connection);
+
+                try
                 {
-                    //Усли есть что считывать то ReadAsync() возвращает true иначе false и выход из блока кода
-                    while (await reader.ReadAsync())
+                    //ExecuteNonQueryAsync() возвращает колличество измененных строк
+                    var succes = await command.ExecuteNonQueryAsync(); 
+                    
+                    if (succes > 0)
                     {
-                        var person = new Person
-                        {
-                            Id = reader.GetInt32(0),
-                            Name = reader.GetString(1),
-                            Age = reader.GetInt32(2),
-                            Height = reader.GetDouble(3)
-                        };
-                        People.Add(person);
+                        MessageBox.Show($"Succes: {succes}");
                     }
-                }     
+                    else
+                    {
+                        MessageBox.Show("Fail");
+                    }
+                }
+                
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
             }
            
         }
