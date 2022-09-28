89ec18db3de915906fd79786c7e80ff197fe54ff
sofoyan gevork <aab81@bk.ru>
2022-09-28 15:58:34 +0300

    dataset

---
+++ AdoNetDataSet/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDataSet
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public DelegateCommand ClickCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            ClickCommand = new DelegateCommand(OnClick);
+        }
+
+        private /*async*/ void OnClick()
+        {
+            string connectionString = "Server=S-DEV-01; Database=AdoExplanationDb; Trusted_Connection=True; Encrypt=false";
+
+            string sql = "Select * FROM Persons";
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                //await connection.OpenAsync();
+
+                SqlDataAdapter dataAdapter = new(sql, connection);
+
+                DataSet dataSet = new();
+
+                dataAdapter.Fill(dataSet);
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
